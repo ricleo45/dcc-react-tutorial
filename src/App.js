8bc53a0ef92952a183d614dc8626d9966f05cb94
ricardo <ricleo45@yahoo.com>
2022-08-06 22:16:32 -0600

    'created input and updates to addNewEntry functions'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, { useState } from "react";
+import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
+import AddEntryForm from "./Components/AddEntry/AddEntryFrom";
+
 function App() {
+  const [entries, setEntries] = useState([
+    { weight: 175, date: "11-23-2021" },
+    { weight: 200, date: "11-24-2021" },
+  ]);
+
+  function addNewEnrty(entry) {
+    let tempEntries = [entry, ...entries];
+
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
-      <h3>Hello World</h3>
+      <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntryProperty={addNewEnrty} />
     </div>
   );
 }
